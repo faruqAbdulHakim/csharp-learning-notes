27c345934c3a0d3eda0f4f556b7c1b3d59a2d0be
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2022-09-02 18:03:44 +0700

    source code pertemuan 1

---
+++ sourcecode/001_PengenalanCSharp/Program.cs
@@ -0,0 +1,173 @@
+﻿namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // print()
+            //Console.WriteLine("PBO B");
+            //Console.Write("PBO");
+            //Console.Write("B");
+
+
+            // input("Masukkan nama: ")
+            // Console.ReadLine();
+            // Console.Write("Masukkan Nama: ");
+
+            ////// Variabel
+            // tipeData namaVariabel = isiData;
+            //string mataKuliah = "PBO";
+
+            //int tinggiBadan;
+            //tinggiBadan = 170;
+            //Console.WriteLine(mataKuliah);
+
+            ////// Tipe Data
+            // bilangan bulat
+            // paling sering digunakan: int, long
+            //int a = 0123;
+            //long b = 11113;
+
+            // bilangan desimal
+            // paling sering digunakan: double
+            //double tinggiBadan = 168.5;
+            //decimal beratBadan = 82.5m;
+
+            // teks
+            // string char
+            //string a = "aaaaa";
+            //char b = 'b';
+
+            // boolean
+            //bool lapar = true; // pengkondisian, perulangan
+
+            //// Operator
+            int a = 5;
+            int b = 3;
+            //Console.WriteLine(a + b); // 8
+            //Console.WriteLine(a - b); // 2
+            //Console.WriteLine(a * b); // 15
+            //Console.WriteLine(a / b); // 1
+            //Console.WriteLine(a % b); // 2
+
+            // print a dulu, setalah itu +1
+            //Console.WriteLine(a++);
+            //Console.WriteLine(a);
+
+            //b--;
+            //Console.WriteLine(b);
+
+            // operator assignment
+            //int c = 4;
+
+            //c = c + 3;
+            //c += 3;
+
+            // operator komparasi
+            //int z = 1;
+            //int y = 1;
+            //int x = 2;
+            //Console.WriteLine(z == y); // true
+            //Console.WriteLine(z != y); // false
+            //Console.WriteLine(z > x); // false
+            //Console.WriteLine(z < x); // true
+            //Console.WriteLine(z >= y); //true
+            //Console.WriteLine(z <= y); // true
+
+            // operator logika
+            // and, or, not
+            // &&, ||, !
+            //Console.WriteLine(true && true); // true
+            //Console.WriteLine(true && false); // false
+            //Console.WriteLine(false && false); // false
+
+            //Console.WriteLine(true || true); // true 
+            //Console.WriteLine(true || false); // true
+            //Console.WriteLine(false || false); // false
+
+            //Console.WriteLine(!true); // false
+
+            //Console.WriteLine(true || false && false);
+            //byte nilai = 60;
+
+            //if (nilai >= 80)
+            //{
+            //    Console.WriteLine("Sempurna");
+            //}
+            //else if (nilai >= 60)
+            //{
+            //    Console.WriteLine("Cukup Baik");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Buruk");
+            //}
+
+            //if (nilai > 50)
+            //{
+            //    "LULUS";
+            //}
+            //else
+            //{
+            //    "TIDAK lULUS";
+            //}
+            //// Ternary Operation
+            //char grade = nilai > 50 ? 'A' : 'B';
+            //Console.WriteLine(grade);
+
+            //string makanan = "Ayam Goreng";
+            //switch (makanan)
+            //{
+            //    // makanan == "Ayam goreng"
+            //    case "Ayam goreng":
+            //        Console.WriteLine("Makan ayam goreng");
+            //        break;
+            //    // makanan == "Ayam Bakar"
+            //    case "Ayam Bakar":
+            //        Console.WriteLine("Makan ayam Bakar");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Aku tidak makan");
+            //        break;
+            //}
+
+            // Perulangan
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            // while
+            //int i = 0;
+            //while (false)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            // dijalankan setidaknya satu kali
+            // meskipun kondisi pertama false
+            //int i = 0;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //while (i < -5);
+
+            // break/continue
+            //for (int i = 0; i < 7; i++)
+            //{
+            //    if (i == 3)
+            //    {
+            //        continue;
+            //    }
+            //    else if (i == 5)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine(i);// 0 1 2 4
+            //}
+        }
+    }
+}
